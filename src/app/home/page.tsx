20ca03e31a2ccfc54543bfd9c32961b363cb0fa1
JuscelinoMamede <juscemamede01@gmail.com>
2023-12-17 14:49:17 -0300

    criação da table da home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,133 +1,79 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { api } from "../../../services/api";
+
+interface LeadProps {
+  id: 1;
+  name: string;
+  telefone: string;
+  email: string;
+}
+
 const HomePage = () => {
+  const [leads, setLeads] = useState([] as LeadProps[]);
+  const [loading, setloading] = useState(false);
+
+  async function loadLeads() {
+    setloading(true);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    try {
+      const response = await api.get("/leads");
+      setLeads(response.data);
+      console.log("Success:", response);
+    } catch (error) {
+      console.log("Error:", error);
+      alert("Ocorreu um erro ao tentar se conectar com o servidor.");
+    } finally {
+      setloading(false);
+    }
+  }
+
+  useEffect(() => {
+    loadLeads();
+  }, []);
+
   return (
     <>
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg text-center border space-y-10 p-4">
-        <h3 className="text-3xl font-bold ">Leads Cadastrados</h3>
+        <h3 className="text-3xl font-bold  border-solid border-b-1">
+          Leads Cadastrados
+        </h3>
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 ">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                Nome
-              </th>
-              <th scope="col" className="px-6 py-3">
-                <div className="flex items-center">
-                  Telefone
-                  <a href="#">
-                    <svg
-                      className="w-3 h-3 ms-1.5"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
-                    </svg>
-                  </a>
-                </div>
-              </th>
-
-              <th scope="col" className="px-6 py-3">
-                <div className="flex items-center">
-                  Email
-                  <a href="#">
-                    <svg
-                      className="w-3 h-3 ms-1.5"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
-                    </svg>
-                  </a>
-                </div>
-              </th>
-              <th scope="col" className="px-6 py-3">
-                <span className="sr-only">Edit</span>
-              </th>
-            </tr>
-          </thead>
           <tbody>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
-              >
-                Roberto Farias
-              </th>
-              <td className="px-6 py-4">(81) 9988-9966</td>
-              <td className="px-6 py-4">rf@gmail.com</td>
-              <td className="px-2 py-4 text-right">
-                <a
-                  href="#"
-                  className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 "
-                >
-                  Editar
-                </a>
-              </td>
-              <td className="px-2 py-4 text-right">
-                <a
-                  href="#"
-                  className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 "
-                >
-                  Delete
-                </a>
-              </td>
-            </tr>
-            <tr className="bg-white border-b ">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
-              >
-                Roberto Farias
-              </th>
-              <td className="px-6 py-4">(81) 9988-9966</td>
-              <td className="px-6 py-4">rf@gmail.com</td>
-              <td className="px-2 py-4 text-right">
-                <a
-                  href="#"
-                  className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 "
-                >
-                  Editar
-                </a>
-              </td>
-              <td className="px-2 py-4 text-right">
-                <a
-                  href="#"
-                  className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 "
-                >
-                  Delete
-                </a>
-              </td>
-            </tr>
-            <tr className="bg-white border-b ">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
-              >
-                Roberto Farias
-              </th>
-              <td className="px-6 py-4">(81) 9988-9966</td>
-              <td className="px-6 py-4">rf@gmail.com</td>
-              <td className="px-2 py-4 text-right">
-                <a
-                  href="#"
-                  className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 "
-                >
-                  Editar
-                </a>
-              </td>
-              <td className="px-2 py-4 text-right">
-                <a
-                  href="#"
-                  className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 "
-                >
-                  Delete
-                </a>
-              </td>
-            </tr>
+            <ul>
+              {leads.map((lead) => (
+                <li key={lead.id}>
+                  <tr className="bg-white border-b ">
+                    <th
+                      scope="row"
+                      className=" font-medium text-gray-900 whitespace-nowrap w-40 py-4"
+                    >
+                      {lead.name}
+                    </th>
+                    <td className="w-40 py-4">{lead.telefone}</td>
+                    <td className=" w-40 py-4">{lead.email}</td>
+                    <td className="w-28 py-4">
+                      <a
+                        href="#"
+                        className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 "
+                      >
+                        Editar
+                      </a>
+                    </td>
+                    <td className="w-28 py-4 ">
+                      <a
+                        href="#"
+                        className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 "
+                      >
+                        Delete
+                      </a>
+                    </td>
+                  </tr>
+                </li>
+              ))}
+            </ul>
           </tbody>
         </table>
       </div>
